0116e4af9794f1239167bf76e48ca8bae8e48d8d
michaelagubovska <michaela.gubovska@gmail.com>
2023-08-21 18:37:45 +0200

    redesign

--- src/web.js
+++ src/web.js
@@ -1,42 +1,56 @@
-mybutton = document.getElementById("topBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
-function changeBackcolor() {
-   document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.7)";
-}
-
-
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-55px";
+    if ($(window).width() > 600) {
+      document.getElementById("navbar").style.top = "-70px";
+    } else {
+      document.getElementById("navbar").style.top = "-96px";
+    }
   }
   prevScrollpos = currentScrollPos;
+};
 
-  if ($(window).width() < 768) {
-	  document.getElementById("navbar").style.backgroundColor = "transparent";
-	  $('.navbar-collapse').collapse('hide');
-	}
-}
-
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
 });
+
+window.onload = function () {
+  $(".work-section__card-spacer").each(function (spacer) {
+    $(this).height($(".work-section__card-mockup--static").height() + 12);
+  });
+};
+
+$(".nav-link").on("click", function () {
+  $(".active").removeClass("active");
+  $(this).addClass("active");
+});
+
+var selector = ".nav-item a";
+$(window).scroll(function () {
+  if (screen.width > 768) {
+    onScrollt(selector);
+  }
+});
+
+function onScrollt(selector) {
+  var scrollPos = $(document).scrollTop();
+  $(selector).each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.length) {
+      if (
+        parseInt(refElement.position().top) <= scrollPos + 250 &&
+        parseInt(refElement.position().top) + parseInt(refElement.height()) >
+          scrollPos + 250
+      ) {
+        currLink.removeClass("active");
+        currLink.addClass("active");
+      } else {
+        currLink.removeClass("active");
+      }
+    }
+  });
+}
